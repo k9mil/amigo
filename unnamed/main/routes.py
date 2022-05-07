55f832870f68f844b547e09984790d9174459cd3
Kamil Zak <kamilzak00@gmail.com>
2022-05-07 18:29:00 +0100

    feat: add /chat/

--- unnamed/main/routes.py
+++ unnamed/main/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route("/chat")
+def chat():
+    return render_template("chat.html")
