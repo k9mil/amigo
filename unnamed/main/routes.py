7c9540dd96f9276fc39a6902561e03f271dc6b44
Kamil Zak <kamilzak00@gmail.com>
2022-05-09 10:36:20 +0100

    feat: add game not found error

--- unnamed/main/routes.py
+++ unnamed/main/routes.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template, Blueprint, redirect, request
 from flask_socketio import send, emit
 from unnamed.config import Config
 from urllib.parse import urlencode
+from furl import furl
 from .. import socketio
 
 import re, urllib, json
 
 main = Blueprint("main", __name__)
 
+clients = []
+
 @socketio.on("message")
 def handleMessage(msg):
-    print(msg)
     send(msg, broadcast = True)
 
 @socketio.on("connect")
-def test_connect():
+def connect():
+    clients.append(request.sid)
+    print(clients)
     emit('my response', {'data': 'Connected'})
+
+@socketio.on("disconnect")
+def disconnect():
+    clients.remove(request.sid)
 
 @main.route("/")
 def index():
@@ -23,22 +31,22 @@ def index():
 
 @main.route("/waiting")
 def waiting():
+    steam_id_re = re.compile("https://steamcommunity.com/openid/id/(.*?)$")
+    match = steam_id_re.search(dict(request.args)["openid.identity"])
+    steam_data = get_user_info(match.group(1))
+
+    try:
+        if (steam_data["gameextrainfo"]):
+            print(steam_data["gameextrainfo"])
+        elif (steam_data["gameid"]):
+            print(steam_data["gameid"])
+    except KeyError:
+        return render_template("errors/game_not_found.html")
+        
     return render_template("waiting.html")
 
 @main.route("/chat")
 def chat():
-    try:
-        steam_id_re = re.compile("https://steamcommunity.com/openid/id/(.*?)$")
-        match = steam_id_re.search(dict(request.args)["openid.identity"])
-        q = match.group(1)
-        print(q)
-        steam_data = get_user_info(q)
-        print(steam_data)
-        what = steam_data["steamid"]
-        print(what)
-    except KeyError:
-        return render_template("chat.html")
-
     return render_template("chat.html")
 
 @main.route("/authenticate")
@@ -50,18 +58,16 @@ def authenticate():
         "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
         "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
         "openid.mode": "checkid_setup",
-        "openid.return_to": "http://127.0.0.1:5000/chat",
+        "openid.return_to": "http://127.0.0.1:5000/waiting",
         "openid.realm": "http://127.0.0.1:5000"
     }
 
     auth_url = openid_url + "?" + urlencode(params)
-    print(auth_url)
 
     return redirect(auth_url)
 
 def get_user_info(steam_id):
     url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=40B868AA0378A7783543346141620CB4&steamids={steam_id}"
-    print(url)
     rv = json.load(urllib.request.urlopen(url))
 
     return rv["response"]["players"][0]
