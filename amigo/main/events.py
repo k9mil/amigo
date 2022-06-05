d05bb4990338fd485564f40e78b080eaee277261
Kamil Zak <kamilzak00@gmail.com>
2022-06-05 18:07:03 +0100

    feat: added room func

--- amigo/main/events.py
+++ amigo/main/events.py
@@ -7,8 +7,8 @@ from .. import socketio
 
 users: list[None] = []
 
-@socketio.on("connect")
-def connect() -> None:
+@socketio.on("redirect")
+def redirect() -> None:
     """This function handles the main connection, and essentially only runs in the waiting room,
     as when it runs on the /chat/ page, there's two exceptions and a pass is thrown. Architecture will be improved.
 
@@ -22,21 +22,22 @@ def connect() -> None:
         None
     """
 
-    try:
-        redis_conn.hset(session["steam_id"], "sid", request.sid)
+    redis_conn.hset(session["steam_id"], "sid", request.sid)
 
-        for k in redis_conn.scan_iter("*"):
-            current_game: str = redis_conn.hget(k, "game").decode('utf-8')
-            current_steam_id: int = session["steam_id"]
-            current_sid: int = redis_conn.hget(k, "sid").decode('utf-8')
+    for k in redis_conn.scan_iter("*"):
+        current_game: str = redis_conn.hget(k, "game").decode('utf-8')
+        current_sid: int = redis_conn.hget(k, "sid").decode('utf-8')
 
-            if current_game == session.get("game") and str(current_steam_id) != str(k.decode('utf-8')):
-                users.append(current_sid)
-                users.append(request.sid)
-                
-                emit("redirect", {"url": url_for("main.chat")}, to=users)
-    except Exception:
-        pass
+        current_steam_id: int = session["steam_id"]
+
+        if current_game == session.get("game") and str(current_steam_id) != k.decode('utf-8'):
+            users.append(current_sid)
+            users.append(request.sid)
+
+            redis_conn.hset(session["steam_id"], "room", request.sid + current_sid)
+            redis_conn.hset(k.decode('utf-8'), "room", request.sid + current_sid)
+
+            emit("redirect", {"url": url_for("main.chat")}, to=users)
 
 @socketio.on("disconnect")
 def disconnect() -> None:
@@ -49,9 +50,9 @@ def disconnect() -> None:
         None
     """
 
-    redis_conn.delete(session["steam_id"])
-    session.pop("steam_id", None)
-    redis_conn.flushall()
+    # redis_conn.delete(session["steam_id"])
+    # session.pop("steam_id", None)
+    # redis_conn.flushall()
 
 @socketio.on("join")
 def join() -> None:
@@ -64,7 +65,7 @@ def join() -> None:
         None
     """
 
-    session["room"] = "room"
+    session["room"] = redis_conn.hget(session["steam_id"], "room").decode('utf-8')
     room: str = session.get("room")
     join_room(room)
 
